010f994b75461d694136d94966f81438e3a50469
lkhorasandzhian <levon.kh228@gmail.com>
2023-11-30 16:07:29 +0300

    list output test was added, previous tests were fixed via prettifying output

--- Testing/Tests.fs
+++ Testing/Tests.fs
@@ -6,18 +6,24 @@ open Interpreter
 
 [<Fact>]
 let ``Testing of easy calculation`` () =
-    let test =  Parser.ParseString("(+ 2 2)")
+    let test =  Parser.ParseString("(+ 2.5 2)")
     let result = Interpreter.launch test
-    Assert.Equal(result |> string, "AstList [AstNumber 4.0]")
+    Assert.Equal(result |> string, "4.5")
 
 [<Fact>]
 let ``Testing of difficult calculation`` () =
     let test =  Parser.ParseString("(/ (* (+ 10 15) (- 7 3)) 2.5)")
     let result = Interpreter.launch test
-    Assert.Equal(result |> string, "AstList [AstNumber 40.0]")
+    Assert.Equal(result |> string, "40")
 
 [<Fact>]
 let ``Testing of logical and mathematical operators`` () =
     let test =  Parser.ParseString("(or (and (= (+ (+ 1 2) (- 8 6)) 5) true) false)")
     let result = Interpreter.launch test
-    Assert.Equal(result |> string, "AstList [AstBool true]")
+    Assert.Equal(result |> string, "True")
+
+[<Fact>]
+let ``Testing of list output`` () =
+    let test =  Parser.ParseString("( 1 2 3 4 5 )")
+    let result = Interpreter.launch test
+    Assert.Equal(result |> string, "( 1 2 3 4 5 )")
