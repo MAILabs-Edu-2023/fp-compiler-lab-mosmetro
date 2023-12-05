7bdd5598e79c1c58f613144857e83c0a81be6b50
Nick Biryulin <keddad@yandex.ru>
2023-12-05 21:33:44 +0300

    Do not reuse AstTypes in Inteprepter

--- src/Interpreter/Interpreter.fs
+++ src/Interpreter/Interpreter.fs
@@ -4,84 +4,104 @@ open Parser
 
 module Interpreter = 
 
+    type IntepretedType = 
+        | MBool of bool
+        | MNumber of float
+        | MString of string
+        | MList of IntepretedType list
+
+    let rec private interpretedTypeToString = function
+        | MBool(b) -> if b then "True" else "False"
+        | MNumber(n) -> n.ToString()
+        | MString(s) -> s
+        | MList(l) -> "(" + (l |> List.map(fun el -> interpretedTypeToString el) |> String.concat " ")  + ")"
+
+    type Variable = {value: IntepretedType}
+    type Function = {args: string list; ast: Parser.Ast}
+
     // Operators translation.
     let private funof = function
         | "+" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> AstNumber(a + b)
-                    | [AstString(a); AstString(b)] -> AstString(a + b)
-                    | [AstNumber(a); AstString(b)] -> AstString(string a + b)
-                    | [AstString(a); AstNumber(b)] -> AstString(a + string b)
+                    | [MNumber(a); MNumber(b)] -> MNumber(a + b)
+                    | [MString(a); MString(b)] -> MString(a + b)
+                    | [MNumber(a); MString(b)] -> MString(string a + b)
+                    | [MString(a); MNumber(b)] -> MString(a + string b)
                     | _ -> failwith "Invalid arguments for addition")
         | "-" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> AstNumber(a - b)
+                    | [MNumber(a); MNumber(b)] -> MNumber(a - b)
                     | _ -> failwith "Invalid arguments for subtraction")
         | "*" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> AstNumber(a * b)
+                    | [MNumber(a); MNumber(b)] -> MNumber(a * b)
                     | _ -> failwith "Invalid arguments for multiplication")
         | "/" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> AstNumber(a / b)
+                    | [MNumber(a); MNumber(b)] -> MNumber(a / b)
                     | _ -> failwith "Invalid arguments for division")
         | "=" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> if a = b then AstBool(true) else AstBool(false)
+                    | [MNumber(a); MNumber(b)] -> if a = b then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for equality check")
         | ">" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> if a > b then AstBool(true) else AstBool(false)
+                    | [MNumber(a); MNumber(b)] -> if a > b then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for greater than check")
         | "<" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> if a < b then AstBool(true) else AstBool(false)
+                    | [MNumber(a); MNumber(b)] -> if a < b then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for less than check")
         | "<=" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> if a <= b then AstBool(true) else AstBool(false)
+                    | [MNumber(a); MNumber(b)] -> if a <= b then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for less than or equal to check")
         | ">=" -> (function 
-                    | [AstNumber(a); AstNumber(b)] -> if a >= b then AstBool(true) else AstBool(false)
+                    | [MNumber(a); MNumber(b)] -> if a >= b then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for greater than or equal to check")
         | "or" -> (function 
-                    | [AstBool(a); AstBool(b)] -> if (a || b) then AstBool(true) else AstBool(false)
+                    | [MBool(a); MBool(b)] -> if (a || b) then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for or to check")
         | "and" -> (function 
-                    | [AstBool(a); AstBool(b)] -> if (a && b) then AstBool(true) else AstBool(false)
+                    | [MBool(a); MBool(b)] -> if (a && b) then MBool(true) else MBool(false)
                     | _ -> failwith "Invalid arguments for and to check")
         | _ -> failwith "Unsupported operator was given"
 
-    // Ability to cast all types to string for console output.
-    let rec public AstToString = function
-    | AstBool(b) -> string b
-    | AstNumber(n) -> string n
-    | AstList(l) -> "(" + (List.fold (fun acc item -> acc + AstToString item + " ") " " l) + ")"
-    | AstString(s) | AstKeyword(s) | AstVariable(s) -> s  // Already string types.
-
     // Main interpreter logic.
-    let rec private eval exp env =
+    let rec private eval exp variableEnv =
         match exp with
-        | AstBool(b) -> AstBool(b)
-        | AstNumber(n) -> AstNumber(n)
-        | AstString(s) -> AstString(s)
+        | AstBool(b) -> MBool(b)
+        | AstNumber(n) -> MNumber(n)
+        | AstString(s) -> MString(s)
+
         | AstVariable(x) ->
-            (match Map.tryFind x env with
+            (match Map.tryFind x variableEnv with
              | Some(value) -> value
              | None -> failwith "Variable not found")
+
         | AstList(lst) ->
-            (match lst with
-             | AstKeyword("let") :: varName :: rawOperators :: innerCodeArea ->
-                let value = eval rawOperators env
-                eval (AstList innerCodeArea) (Map.add (AstToString varName) value env)
-             | AstVariable(operation) :: elements ->
-                if elements.IsEmpty then
-                    eval (AstVariable operation) env
-                else
-                    let astResults = List.map (fun item -> eval item env) elements
-                    funof operation astResults
-             | AstKeyword("if") :: condition :: trueBranch :: falseBranch :: [] ->
-                 let evalCondition = eval condition env
-                 if evalCondition = AstBool(true) then eval trueBranch env
-                 else eval falseBranch env
-             | _ -> AstList(List.map (fun item -> eval item env) lst))
+            (
+                match lst with
+                | AstKeyword("let") :: varName :: rawOperators :: innerCodeArea ->
+                    let value = eval rawOperators variableEnv
+                    
+                    match varName with
+                        | AstVariable(varName) -> eval (AstList innerCodeArea) (Map.add varName value variableEnv)
+                        | _ -> failwith "Variable name must be AstVariable"
+
+                | AstKeyword("if") :: condition :: trueBranch :: falseBranch :: [] ->
+                    let evalCondition = eval condition variableEnv
+                    
+                    if evalCondition = MBool(true) then 
+                        eval trueBranch variableEnv
+                    else 
+                        eval falseBranch variableEnv
+
+                | AstVariable(operation) :: elements ->
+                    if elements.IsEmpty then
+                        eval (AstVariable operation) variableEnv
+                    else
+                        let astResults = List.map (fun item -> eval item variableEnv) elements
+                        funof operation astResults
+
+                | _ -> MList(List.map (fun item -> eval item variableEnv) lst)
+            )
+
         | _ -> failwith "Undefined behaviour"
 
     let public Launch = fun tree ->
         let evalRes = eval tree Map.empty
 
-        match evalRes with
-        | AstList(lst) -> AstToString lst.Head
-        | _ -> failwith "Interpretation errorW"
+        interpretedTypeToString evalRes
